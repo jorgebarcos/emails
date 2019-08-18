058c7e8d1f48b6ff5f1cdbb4f5a2c47c4efd4b22
Jorge Barcos <jorgebarcos88@gmail.com>
2019-08-18 10:08:26 -0300

    Validando que los campos no estén vacios

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ eventListeners();
 function eventListeners() {
 	// Inicio de la aplicación y deshabilitar submit
 	document.addEventListener('DOMContentLoaded', inicioApp);
+	// Campos del formulario
+	email.addEventListener('blur', validarCampo);
+	asunto.addEventListener('blur', validarCampo);
+	mensaje.addEventListener('blur', validarCampo);
 }
 
 // Funciones
@@ -17,4 +21,52 @@ function inicioApp() {
 	// deshabilitar el envio
 
 	btnEnviar.disabled = true;
+}
+// Valida que el campo tenga algo escrito
+
+function validarCampo() {
+	// Se valida la longitud del texto y que no este vacio
+	validarLongitud(this);
+
+	// Validar unicamente el email
+	if (this.type === 'email') {
+		validarEmail(this);
+	}
+	let errores = document.querySelectorAll('.error');
+	if (email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+		if (errores.length === 0) {
+			btnEnviar.disabled = false;
+		}
+	}
+}
+function validarLongitud(campo) {
+	if (campo.value.length > 0) {
+		campo.style.borderBottomColor = 'green';
+		campo.classList.remove('error');
+	} else {
+		campo.style.borderBottomColor = 'red';
+		campo.classList.add('error');
+	}
+}
+
+/*function validarEmail(campo) {
+	const mensaje = campo.value;
+	if (mensaje.indexOf('@') !== -1) {
+		campo.style.borderBottomColor = 'green';
+		campo.classList.remove('error');
+	} else {
+		campo.style.borderBottomColor = 'red';
+		campo.classList.add('error');
+	}
+}
+*/
+function validarEmail(campo) {
+	const mail = campo.value;
+	const validacion = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i; // expresión para validar correo
+
+	if (validacion.test(mail)) {
+		(campo.style.borderBottomColor = 'green'), campo.classList.remove('error');
+	} else {
+		(campo.style.borderBottomColor = 'red'), campo.classList.add('error');
+	}
 }
